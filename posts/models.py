ef16e56f768e287f93b2af21402d8e9a3695ae64
juanmunoz22-bit <63082861+juanmunoz22-bit@users.noreply.github.com>
2021-10-27 23:12:47 -0500

    model posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    bio = models.TextField(max_length=500, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+
+    is_admin = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
