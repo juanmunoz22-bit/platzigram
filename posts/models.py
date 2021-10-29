0bc146f1fb540ca93761c068d7ae9585ad15c452
juanmunoz22-bit <63082861+juanmunoz22-bit@users.noreply.github.com>
2021-10-28 23:17:24 -0500

    v0.1
    Posts and Profile models created.
    Admin url configured for the models above.

--- posts/models.py
+++ posts/models.py
@@ -1,19 +1,20 @@
+#django
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from users.models import Profile
 
-class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    bio = models.TextField(max_length=500, blank=True)
-    birthdate = models.DateField(null=True, blank=True)
-    password = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
+class Post(models.Model):
 
-    is_admin = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+    
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.email
+        return '{} by @{}'.format(self.title, self.user.username)
